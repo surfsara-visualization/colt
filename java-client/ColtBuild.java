b877544fbc516aa2f7bcdc8d8f115dff4e6fc7e7
Paul Melis <paul@floorball-flamingos.nl>
2013-09-26 14:17:22 +0200

    Don't store build file in repo

--- java-client/ColtBuild.java
+++
@@ -1,6 +0,0 @@
-
-public class ColtBuild
-{
-    public final static String revision = "59fa9cf9c87227355ae8f3fed0e50147a85cb1e8+";
-    public final static String build_date = "Thu Sep 26 14:09:02 2013";
-}
